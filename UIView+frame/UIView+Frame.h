48d738bad5b993ffaf1758c5866df2ca618bb9a7
jerome00 <zhaojj9043@gmail.com>
2017-03-22 00:31:27 +0800

    UIView+Frame

---
+++ UIView+frame/UIView+Frame.h
@@ -0,0 +1,26 @@
+//
+//  UIView+Frame.h
+//  手势拖拽侧边栏
+//
+//  Created by JJ on 15/8/17.
+//  Copyright (c) 2015年 jj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Frame)
+
+@property(nonatomic,assign)CGFloat x;
+
+@property(nonatomic,assign)CGFloat y;
+
+@property(nonatomic,assign)CGPoint origin;
+
+@property(nonatomic,assign)CGSize size;
+
+@property(nonatomic,assign)CGFloat width;
+
+@property(nonatomic,assign)CGFloat height;
+
+
+@end
